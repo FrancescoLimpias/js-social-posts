c062ce09a66671ff639a65c9514395f047b70ade
Francesco Limpias <francescolimpias@gmail.com>
2022-11-23 17:34:00 +0100

    Bonus 2

--- main.js
+++ main.js
@@ -19,17 +19,26 @@ postsList.forEach((post, index) => {
         postElement.querySelector(selector)[property] = value;
     }
 
-    // build postElement
+    // Build postElement
     setValue(".post-meta__author", "innerHTML", post.author.name);
+
+    // set profile
     if (post.author.image) {
         setValue(".profile-pic", "src", post.author.image);
     } else {
-        // X BONUS
-        /* const fullName = 
-        setValue(".post-meta__icon", "innerHTML", `${post.author.name.split("")}`); */
+        const names = post.author.name.split(" ");
+        // add default profile name
+        setValue(".post-meta__icon", "innerHTML",
+            `<div class="profile-pic-default">
+                <span>${names[0][0]} ${names[1][0]}</span>
+            </div>`);
     }
+
+    // set date
     const dSplit = post.created.split("-"); // just a lazy js Date format
     setValue(".post-meta__time", "innerHTML", `${dSplit[2]}-${dSplit[1]}-${dSplit[0]}`);
+
+    // set everything else
     setValue(".post__text", "innerHTML", post.content);
     setValue(".post__image img", "src", post.media);
     likeButtonElement.setAttribute("data-postid", post.id);
@@ -38,12 +47,12 @@ postsList.forEach((post, index) => {
 
     // like button interaction
     likeButtonElement.addEventListener("click", () => {
-        if(!likesList.includes(post.id)){
+        if (!likesList.includes(post.id)) {
             likeButtonElement.classList.add("like-button--liked");
             likeCounterElement.innerHTML = post.likes + 1;
             likesList.push(post.id);
         }
-    }); 
+    });
 
     // add postElement to DOM
     postsListElement.append(postElement);
